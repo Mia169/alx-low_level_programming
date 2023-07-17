ee48cb84fec0c7eb72e20a5a76464f5df45011ce
Mia169 <bersabeh.girma011@gmail.com>
2023-07-17 09:42:53 +0300

    final

---
+++ 0x18-dynamic_libraries/main.h
@@ -0,0 +1,37 @@
+<<<<<<< HEAD
+#ifndef _MAIN_H_
+#define _MAIN_H_
+=======
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+>>>>>>> 47a489673b8ca50440efc65a4e2dcf8471f8609f
+
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int n);
+int _isupper(int c);
+int _isdigit(int c);
+int _strlen(char *s);
+void _puts(char *s);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+
+<<<<<<< HEAD
+#endif /* _MAIN_H_ */
+=======
+#endif /* MAIN_H */
+>>>>>>> 47a489673b8ca50440efc65a4e2dcf8471f8609f
